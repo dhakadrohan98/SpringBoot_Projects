70386c75d60a3c8966c18ebd50455a06b5759ab7
dhakadrohan98 <rohandhakad20@gmail.com>
2024-06-28 19:10:35 +0530

    Employee management service were implemented using Spring REST API & Spring DATA Jpa modules, JPQL is written in most of queries

---
+++ spring-data-jpa/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into EMPLOYEE values(25, 1, 25000, 'Tester', 'Tanuja');
+insert into EMPLOYEE values(22, 2, 35000, 'Programmer', 'Prashant');
+insert into EMPLOYEE values(35, 3, 30000, 'Programmer', 'Pradeep');
+insert into EMPLOYEE values(45, 4, 95000, 'Manager', 'Mamta');
+insert into EMPLOYEE values(28, 5, 30000, 'Tester', 'Tilak');
+insert into EMPLOYEE values(31, 6, 75000, 'Programmer', 'Poornima');
+insert into EMPLOYEE values(52, 7, 85000, 'Manager', 'Mahendra');
+insert into EMPLOYEE values(26, 8, 25000, 'Tester', 'Tejaswini');
